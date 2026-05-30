8ec0ab4a84c926e5e1f49462f1f32029f94d7a77
Abosede Oloude <oloudeabosede26@gmail.com>
2026-05-30 09:58:28 +0100

    fixed changes

--- src/components/HomePage/PartnersSection.jsx
+++ src/components/HomePage/PartnersSection.jsx
@@ -1,4 +1,35 @@
+import { useCallback, useEffect, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+
+const partners = [
+  "/p1.png",
+  "/p2.png",
+  "/p3.png",
+  "/p2.png",
+  "/p1.png",
+  "/p3.png",
+];
+
 function Partners() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    align: "start",
+    loop: true,
+  });
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const onSelect = useCallback(() => {
+    if (!emblaApi) return;
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, [emblaApi]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    emblaApi.on("select", onSelect);
+    onSelect();
+  }, [emblaApi, onSelect]);
+
   return (
     <section className="invest-bg bg-green100! py-20  px-4 md:px-10 lg:px-20 flex flex-col gap-12">
       <div className="flex flex-col gap-2 items-center">
@@ -9,11 +40,31 @@ function Partners() {
           Working together to transform Lagos's economy
         </p>
       </div>
-      <div className="grid grid-cols-2 gap-3 md:grid-cols-4">
-        <img src="/p1.png" className="w-full" />
-        <img src="/p2.png" className="w-full" />
-        <img src="/p3.png" className="w-full" />
-        <img src="/p2.png" className="w-full" />
+      {/* Carousel */}
+      <div className="overflow-hidden" ref={emblaRef}>
+        <div className="-ml-5 flex">
+          {partners.map((partner) => (
+            <div
+              key={partner}
+              className="min-w-0 flex-[0_0_100%] pl-5 md:flex-[0_0_33.333%] lg:flex-[0_0_25%] flex justify-center"
+            >
+              <img src={partner} className="object-contain" />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Dots */}
+      <div className="mt-10 flex justify-center gap-3 bg-slate600 shadow-business px-6 py-3 rounded-full w-fit mx-auto">
+        {partners.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => emblaApi?.scrollTo(index)}
+            className={`h-3 w-3 rounded-full transition ${
+              selectedIndex === index ? "bg-green100" : "bg-slate700"
+            }`}
+          />
+        ))}
       </div>
     </section>
   );
